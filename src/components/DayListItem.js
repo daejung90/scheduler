53ed54509a482bea92c012ef8231130b33c69816
daejung90 <djung.ca@gmail.com>
2023-01-13 13:21:59 -0500

    Daylist and DayListItem tested and done on the browser with mock data

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,8 +6,8 @@ import "components/DayListItem.scss"
 export default function DayListItem(props) {
 
     const dayClass = classNames("day-list__item", {
-        "day-list_item--selected": props.selected,
-        "day-list_item--full": props.spots <= 0
+        "day-list__item--selected": props.selected,
+        "day-list__item--full": props.spots <= 0
     });
 
     const formatSpots = function(spots) {
