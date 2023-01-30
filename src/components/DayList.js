0d6891ceea954ec1412d33262e7bae0e5aaf79ed
daejung90 <djung.ca@gmail.com>
2023-01-30 12:14:52 -0500

    fixing storybook testing problem in clickable in daylistitem

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -9,7 +9,7 @@ function DayList (props) {
             name={day.name} 
             spots={day.spots} 
             selected={day.name === props.value}
-            onChange={props.onChange}  
+            setDay={props.onChange}
         />     
     );
 
